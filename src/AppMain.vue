a54301a899fe591632499a7fba4e2575211015d2
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-15 18:48:11 +0100

    Creato una base di card

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -15,25 +15,33 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        <ul>
-            <li>{{store.film[0].title}}</li>
-            <li>{{store.film[0].original_title}}</li>
-            <li>{{store.film[0].original_language}}</li>
-            <li>{{store.film[0].vote_average}}</li>
-
-        </ul>
-    </div>
+    <main>
+        <div class="card">
+             <h5>{{store.film[0].title}}</h5>
+             <h5>{{store.film[0].original_title}}</h5>
+             <h5>{{store.film[0].original_language}}</h5>
+             <h5>{{store.film[0].vote_average}}</h5>
+        </div>
+    </main>
 </template>
 
 <style lang="scss">
-ul {
-    margin: 20px;
+main {
+    max-width: 1200px;
+    margin: 30px auto;
+    display: flex;
+    flex-wrap: wrap;
 
-    li {
-        color: white;
-        font-size: medium;
+    .card {
+        width: calc(100% / 4 - 10px);
+        margin: 5px;
+        text-align: center;
+
+        h5 {
+            color: white;
+        }
 
     }
+
 }
 </style>
