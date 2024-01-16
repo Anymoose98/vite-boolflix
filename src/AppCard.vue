7f8b0d6ee8c0a16e1fa5aa8abab2c8b2e9623205
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-16 19:10:48 +0100

    Creato effetto che gira dopo hover, inizio ad abbellire la pagina

--- src/AppCard.vue
+++ src/AppCard.vue
@@ -11,31 +11,42 @@ export default {
         // Funzione per dividire in due e arrottondare per eccesso 
         calcolaVotoMedio(voto) {
             return Math.ceil(voto / 2);
-        }
+        },
+
+        // bandiera() {
+        //     return src = "https://flagsapi.com/IT/shiny/64.png";
+        // }
     },
 }
 </script>
         
 <template lang="">
+    <div class="card-container">
         <div class="card">
+            <div class="card-front">
+                <h2>{{film.title}}</h2>
 
-            <!-- In caso non esistesse film.backdrop_path prende una foto sennò carica img -->
-            <img v-if="film.backdrop_path == null " class="img-null" alt="" src="https://www.matteopogliani.it/wp-content/plugins/content-cards/skins/default/content-cards-placeholder.png">
-            <img  v-else :src="'https://image.tmdb.org/t/p/original' + film.backdrop_path " class="img-card" alt=""  >
+                <!-- In caso non esistesse film.backdrop_path prende una foto sennò carica img -->
+                <img v-if="film.backdrop_path == null " class="img-null" alt="" src="https://www.matteopogliani.it/wp-content/plugins/content-cards/skins/default/content-cards-placeholder.png">
+                <img  v-else :src="'https://image.tmdb.org/t/p/original' + film.backdrop_path " class="img-card" alt=""  >
+                
+                <!-- Creazione 5 stelle -->
+                <div class="contenitore-stelle">
+                    <span v-for="index in calcolaVotoMedio(film.vote_average)" :key="index" ><i class="fa-solid fa-star"></i></span>
+                    <span v-for="index in 5 - calcolaVotoMedio(film.vote_average)"  :key="index"><i class="fa-regular fa-star"></i></span>
+                </div>
+            </div>
 
-            <h5>{{film.title}}</h5>
-            <h5>{{film.original_title}}</h5>
-
-            <!-- Ricerca bandiera usando trasformandolo in maiuscolo sennò non funziona -->
-            <img :src="'https://flagsapi.com/'+ film.original_language.toUpperCase() +'/shiny/64.png'" class="bandiere" />    
-
-            <!-- Creazione 5 stelle -->
-            <div class="contenitore-stelle">
-                <span v-for="index in calcolaVotoMedio(film.vote_average)" :key="index" ><i class="fa-solid fa-star"></i></span>
-                <span v-for="index in 5 - calcolaVotoMedio(film.vote_average)"  :key="index"><i class="fa-regular fa-star"></i></span>
+            <div class="card-retro">
+                <h5>{{film.title}}</h5>
+                <h5>{{film.original_title}}</h5>
+                
+                <!-- Ricerca bandiera usando trasformandolo in maiuscolo sennò non funziona -->
+                <img :src="'https://flagsapi.com/'+ film.original_language.toUpperCase() +'/shiny/64.png'" class="bandiere" />
             </div>
 
         </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/AppSerie.vue
+++ src/AppSerie.vue
@@ -16,26 +16,31 @@ export default {
 </script>
         
 <template lang="">
-    <div class="card">
+    <div class="card-container">
+        <div class="card">
+            <div class="card-front">
+                <h2>{{serie.name}}</h2>
+                
+                <!-- In caso non esistesse film.backdrop_path prende una foto sennò carica img -->
+                <img v-if="serie.backdrop_path == null " class="img-null" alt="" src="https://www.matteopogliani.it/wp-content/plugins/content-cards/skins/default/content-cards-placeholder.png">
+                <img  v-else :src="'https://image.tmdb.org/t/p/original' + serie.backdrop_path " class="img-card" alt=""  >
 
-             <!-- In caso non esistesse film.backdrop_path prende una foto sennò carica img -->
-             <img v-if="serie.backdrop_path == null " class="img-null" alt="" src="https://www.matteopogliani.it/wp-content/plugins/content-cards/skins/default/content-cards-placeholder.png">
-             <img  v-else :src="'https://image.tmdb.org/t/p/original' + serie.backdrop_path " class="img-card" alt=""  >
-
-             <h5>{{serie.name}}</h5>
-
-             <h5>{{serie.original_name}}</h5>
-
-             <!-- Ricerca bandiera usando trasformandolo in maiuscolo sennò non funziona -->
-             <img :src="'https://flagsapi.com/'+ serie.origin_country[0] +'/shiny/64.png'" class="bandiere"/>    
-
-             <!-- Creazione 5 stelle -->
-             <div class="contenitore-stelle">
-                <span v-for="index in calcolaVotoMedio(serie.vote_average)" :key="index" ><i class="fa-solid fa-star"></i></span>
-                <span v-for="index in 5 - calcolaVotoMedio(serie.vote_average)"  :key="index"><i class="fa-regular fa-star"></i></span>
+                <!-- Creazione 5 stelle -->
+                <div class="contenitore-stelle">
+                   <span v-for="index in calcolaVotoMedio(serie.vote_average)" :key="index" ><i class="fa-solid fa-star"></i></span>
+                   <span v-for="index in 5 - calcolaVotoMedio(serie.vote_average)"  :key="index"><i class="fa-regular fa-star"></i></span>
+                </div>
             </div>
-             <!-- <h5>{{calcolaVotoMedio(serie.vote_average)}}</h5> -->
+    
+            <div class="card-retro">
+                <h5>{{serie.name}}</h5>
+                <h5>{{serie.original_name}}</h5>
+
+                <!-- Ricerca bandiera usando trasformandolo in maiuscolo sennò non funziona -->
+                <img :src="'https://flagsapi.com/'+ serie.origin_country[0] +'/shiny/64.png'" class="bandiere"/>    
+            </div>
         </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
